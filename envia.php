5b9373bd305e94ae86bdfd6ce04d3ec83f674a92
Iuri Pereira <iuri.3d@gmail.com>
2020-09-06 19:50:02 +0100

    project

---
+++ envia.php
@@ -0,0 +1,18 @@
+<?php
+if (! empty ( $_POST ['nome']) && ! empty ( $_POST ['email']) && ! empty ( $_POST ['assunto'])&& ! empty ( $_POST ['texto'])){
+	$corpo = "De: " . $_POST ['nome'] . " ". $_POST ['sobrenome'] . "<" . $_POST ['email'] . " >\n\n";
+	$corpo .= $_POST ['texto'];
+	send_email ("ruyfernandes72@hotmail.com", $_POST ['assunto'], $corpo );
+} else {
+	echo "<script > alert (' Erro de preenchimento !'); history . back () ;</ script >";
+}
+
+function send_email ($para , $assunto , $corpo ){
+	if ( mail ($para , $assunto , $corpo )) {
+		echo "<script > alert (' Mensagem pronta para envio !'); history . back () ;</ script >";
+	} else {
+		echo "<script > alert (' Falha no envio !'); history . back (); </ script >";
+	}
+}
+
+?>
